8dd97fae51d16aee79e34c1dd4cbf4dc4163682c
tomomichelle <ma1n.tmrev01ut10n@gmail.com>
2022-09-20 16:41:54 +0900

    update (add time sleep to prevent 404 error)

--- YouTube.py
+++ YouTube.py
@@ -2,6 +2,7 @@ from ytmusicapi import YTMusic
 from datetime import datetime
 import os
 import re
+import time
 import argparse
 import difflib
 from collections import OrderedDict
@@ -16,7 +17,10 @@ class YTMusicTransfer:
         self.api = YTMusic(settings['youtube']['headers'], settings['youtube']['user_id'])
 
     def create_playlist(self, name, info, privacy="PRIVATE", tracks=None):
-        return self.api.create_playlist(name, info, privacy, video_ids=tracks)
+        res = self.api.create_playlist(name, info, privacy, video_ids=tracks)
+        # prevent 404 error
+        time.sleep(1)
+        return res
 
     def get_best_fit_song_id(self, results, song):
         match_score = {}
@@ -79,6 +83,8 @@ class YTMusicTransfer:
 
             if i > 0 and i % 10 == 0:
                 print(f"YouTube tracks: {i}/{len(songs)}")
+            # prevent 404 error
+            time.sleep(1)
 
         with open(path + 'noresults_youtube.txt', 'w', encoding="utf-8") as f:
             f.write("\n".join(notFound))
@@ -90,9 +96,13 @@ class YTMusicTransfer:
     def add_playlist_items(self, playlistId, videoIds):
         videoIds = OrderedDict.fromkeys(videoIds)
         self.api.add_playlist_items(playlistId, videoIds)
+        # prevent 404 error
+        time.sleep(1)
 
     def get_playlist_id(self, name):
         pl = self.api.get_library_playlists(10000)
+        # prevent 404 error
+        time.sleep(1)
         try:
             playlist = next(x for x in pl if x['title'].find(name) != -1)['playlistId']
             return playlist
@@ -100,28 +110,42 @@ class YTMusicTransfer:
             raise Exception("Playlist title not found in playlists")
 
     def get_library_playlists(self):
-        return self.api.get_library_playlists(10000)
+        res = self.api.get_library_playlists(10000)
+        # prevent 404 error
+        time.sleep(1)
+        return res
 
     def remove_songs(self, playlistId):
         items = self.api.get_playlist(playlistId, 10000)['tracks']
+        # prevent 404 error
+        time.sleep(1)
         if len(items) > 0:
             self.api.remove_playlist_items(playlistId, items)
+            # prevent 404 error
+            time.sleep(1)
 
     def remove_playlists(self, pattern, confirm=False):
         playlists = self.api.get_library_playlists(10000)
+        # prevent 404 error
         p = re.compile("{0}".format(pattern))
         matches = [pl for pl in playlists if p.match(pl['title'])]
         print("The following playlists will be removed:")
         print("\n".join([pl['title'] for pl in matches]))
         if confirm:
-            [self.api.delete_playlist(pl['playlistId']) for pl in matches]
+            for pl in matches:
+                self.api.delete_playlist(pl['playlistId'])
+                # prevent 404 error
+                time.sleep(1)
             print(str(len(matches)) + " playlists deleted.")
             return
 
         print("Please confirm (y/n):")
         choice = input().lower()
         if choice[:1] == 'y':
-            [self.api.delete_playlist(pl['playlistId']) for pl in matches]
+            for pl in matches:
+                self.api.delete_playlist(pl['playlistId'])
+                # prevent 404 error
+                time.sleep(1)
             print(str(len(matches)) + " playlists deleted.")
         else:
             print("Aborted. No playlists were deleted.")
@@ -139,6 +163,7 @@ def get_args():
     parser.add_argument("-r", "--remove", action='store_true', help="Remove playlists with specified regex pattern.")
     parser.add_argument("-a", "--all", action='store_true', help="Transfer all public playlists of the specified user (Spotify User ID).")
     parser.add_argument("-au", "--allupdate", action='store_true', help="Delete all playlists and udpate them with entries from the Spotify playlist.")
+    parser.add_argument("-rx", "--regex", type=str, help="Delete all playlists and update them with regex.")
     parser.add_argument("-ra", "--removeall", action='store_true', help="Delete all playlists.")
     parser.add_argument("-ru", "--removeuser", action='store_true', help="Delete all playlists created by the Spotify user.")
     return parser.parse_args()
@@ -151,6 +176,7 @@ def main():
     if args.removeall:
         for p in ytmusic.get_library_playlists():
             ytmusic.remove_playlists(p['title'], confirm=True)
+
         return
 
     if args.removeuser:
@@ -160,14 +186,24 @@ def main():
             ytmusic.remove_playlists(p['name'], confirm=True)
         return
 
-    if args.all or args.allupdate:
+    if args.all or args.allupdate or args.regex:
         s = Spotify()
         pl = s.getUserPlaylists(args.playlist)
         print(str(len(pl)) + " playlists found. Starting transfer...")
         count = 1
+        if args.regex:
+            print(f"regex: {args.regex}")
         for p in pl:
-            if args.allupdate:
-                ytmusic.remove_playlists(p['name'], confirm=True)
+            if args.regex:
+                if re.compile(args.regex).match(p['name']) is None:
+                    print(f"skip {p['name']}.")
+                    continue
+
+            if args.allupdate or args.regex:
+                try:
+                    ytmusic.remove_playlists(p['name'], confirm=True)
+                except:
+                    pass
             print("Playlist " + str(count) + ": " + p['name'])
             count = count + 1
             try:
